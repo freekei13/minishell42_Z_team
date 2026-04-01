c933cfaeb4aa3ab6f5d7656b20bddbc3d2675a3d
freekei13 <freesamakkasetkan@hotmail.com>
2026-04-02 01:37:13 +0200

    Parser : done | print_ast in progress

--- parser/parsing.c
+++ parser/parsing.c
@@ -6,33 +6,106 @@
 /*   By: csamakka <csamakka@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 18:29:27 by csamakka          #+#    #+#             */
-/*   Updated: 2026/04/01 22:18:34 by csamakka         ###   ########.fr       */
+/*   Updated: 2026/04/02 01:26:34 by csamakka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "parsing.h"
 #include "lexing.h"
 
-// Algorithme du parser
-// - cherche token pipe. si trouve cree un noeud pipe.
-// 	- gauche et droite
-// - si pas de pipe.
-// 	- cree un noeud cmd
+int	lst_word_counter(t_token *tokens)
+{
+	int		word_nb;
+	t_token	*tmp;
+
+	word_nb = 0;
+	tmp = tokens;
+	while (tmp && tmp->type != PIPE)
+	{
+		if (tmp->type == WORD)
+			word_nb++;
+		tmp = tmp->next;
+	}
+	return (word_nb);
+}
+
+void	cmd_node_loop(t_token *tokens, t_ast *node)
+{
+	int	i;
+	
+	i = 0;
+	while (tokens && tokens->type != PIPE)
+	{
+		if (tokens->type == WORD)
+		{
+			node->data.cmd.args[i++] = tokens->value;
+			tokens = tokens->next;
+		}
+		else if (tokens->type == REDIRECT_IN || tokens->type == REDIRECT_OUT
+			|| tokens->type == APPEND || tokens->type == HEREDOC)
+		{
+			add_redirect_back(&node->data.cmd.redirects,
+				new_redirect(tokens->next->value, tokens->type));
+			tokens = tokens->next->next;
+		}
+	}
+	node->data.cmd.args[i] = NULL;
+}
+
+t_ast	*cmd_node_parser(t_token *tokens)
+{
+	t_ast		*node;
+	int			type_word_nb;
+
+	type_word_nb = lst_word_counter(tokens);
+	node = malloc(sizeof(t_ast));
+	if (!node)
+		return (NULL);
+	node->type = AST_CMD;
+	node->data.cmd.args = malloc(sizeof(char *) * (type_word_nb + 1));
+	node->data.cmd.redirects = NULL;
+	cmd_node_loop(tokens, node);
+	return (node);
+}
+
+t_ast	*parser(t_token *tokens);
+
+t_ast	*pipe_node_parser(t_token *tokens)
+{
+	t_token		*prev;
+	t_ast		*node;
+	t_token		*head;
+	
+	head = tokens;
+	node = malloc(sizeof(t_ast));
+	if (!node)
+		return (NULL);
+	node->type = AST_PIPE;
+	while(tokens)
+	{
+		if ( tokens->next == NULL || tokens->next->type == PIPE) 
+			break ;
+		tokens = tokens->next;	
+	}
+	prev = tokens;
+	if (!tokens->next || !tokens->next->next)
+		tokens = NULL;
+	else
+		tokens = tokens->next->next;
+	prev->next = NULL;
+	node->data.pipe.left = parser(head);
+	node->data.pipe.right = parser(tokens);
+	return (node);
+}
 
 t_ast	*parser(t_token *tokens)
 {
-	t_token		*head;
 	int			node_type;
 	t_ast		*node;
-	t_ast		**node_cmd;
-	int			type_word_nb;
-	int			i;
-
-	head = tokens;
+	t_token		*head;
+	
 	node_type = 0;
-	type_word_nb = 0;
-	i = 0;
+	head = tokens;
 	while(tokens)
 	{
 		if (tokens->type == PIPE)
@@ -44,38 +117,8 @@ t_ast	*parser(t_token *tokens)
 	}
 	tokens = head;
 	if (node_type == 0)
-	{
-		while(tokens && tokens->type != PIPE)
-		{
-			if (tokens->type == WORD)
-				type_word_nb++;
-			tokens = tokens->next;
-		}
-		node = malloc(sizeof(t_ast));
-		node->type = AST_CMD;
-		node->data.cmd.args = malloc(sizeof(char *) * (type_word_nb + 1));
-		tokens = head;
-		while (tokens && tokens->type != PIPE)
-		{
-			if (tokens->type == WORD)
-			{
-				node->data.cmd.args[i] = tokens->value;
-				i++;
-				tokens = tokens->next;
-			}
-			else if (tokens->type == REDIRECT_IN || tokens->type == REDIRECT_OUT
-				|| tokens->type == APPEND || tokens->type == HEREDOC)
-			{
-				add_redirect_back(node->data.cmd.redirects,
-					new_redirect(tokens->next->value, tokens->type));
-				tokens = tokens->next->next;
-			}
-		}
-		node->data.cmd.args[i] = NULL;
-	}
+		node = cmd_node_parser(tokens);
 	else
-	{
-		node->type = AST_PIPE;
-	}
+		node = pipe_node_parser(tokens);
 	return (node);
 }
