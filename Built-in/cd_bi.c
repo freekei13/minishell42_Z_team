7b89364a59858cde8a7ff09e387770fc1410c8a9
razgolanaz-sudo <razgolanaz@gmail.com>
2026-05-29 10:44:41 +0200

    cd ../../..

--- Built-in/cd_bi.c
+++ Built-in/cd_bi.c
@@ -3,13 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   cd_bi.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lalamino <lalamino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/15 10:03:07 by lalamino          #+#    #+#             */
-/*   Updated: 2026/05/26 11:20:06 by lalamino         ###   ########.fr       */
+/*   Updated: 2026/05/29 10:29:55 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
 #include "builtin.h"
 
 // int	cdERR(char **args)
@@ -31,11 +32,73 @@ void	pwd_update(char **env, char *path)
 	return ;
 }
 
+void	pwd_dot_update(char **env)
+{
+	char	**pwd;
+	char	*str;
+	char	*cmp;
+	int		i;
+
+	i = -1;
+	str = malloc(sizeof(char *) * ft_strlen(find_env(env, "PWD")));
+	cmp  = malloc(sizeof(char *) * ft_strlen(find_env(env, "PWD")));
+	pwd = malloc(sizeof(char **) * 3);
+	cmp = find_env(env, "PWD");
+	while(cmp[++i] && cmp[i] != '/')
+		str[i] = cmp[i];
+	str[i] = NULL;
+	pwd[0] = ft_strjoin("OLDPWD=", find_env(env, "PWD"));
+	pwd[1] = ft_strjoin("PWD=", str);
+	pwd[2] = NULL;
+	env = chg_env(env, pwd);
+	printf("PWD : %s\nOLDPWD : %s\n\n", pwd[1], pwd[0]);
+	free(str);
+	free(cmp);
+	split_free(pwd);
+	return ;
+}
+
+char	*no_dash(char *arg, int x)
+{
+	char	*str;
+	int		i;
+
+	i = -1;
+	str = malloc(sizeof(char *) * x + 1);
+	while (arg[++i] && arg[i] != '/')
+		str[i] = arg[i];
+	str[i] = NULL;
+	return(str);
+}
+
+int	dot_cd(char **arg, char **env, t_int i)
+{
+	char *str;
+
+	if (arg[1][0] == '.' && arg[1][1] == '.' &&
+		(arg[1][2] == '.' 	|| !arg[1][2] || arg[1][2] == '/'))
+	{
+		i.i = dash_lengh(arg[1]);
+		str = no_dash(arg, i);
+		if (i == -1)
+			i.ks = chdir(arg[1]);
+		else
+			i.ks = chdir(str);
+		if (i.ks == -1)
+			return(1);
+		free(str);
+		pwd_dot_update(env);
+		if (i == -1)
+			return (cd(arg[1] + 3), env);
+		return(0);
+	}
+	return (1);
+}
+
 int		cd(char **args, char **env)
 {
 	t_int	i;
 
-	i.i = -1;
 	if (args_size(args) != 2)
 		return (1);
 	if (args[1] && strncmp(args[1], "-", 2) == 0)
@@ -46,6 +109,8 @@ int		cd(char **args, char **env)
 		else if (i.ks == -1)
 			return (1);
 	}
+	else if (arg[1][0] == '.')
+		return (dot_cd(args, env, i));
 	else
 	{
 		i.ks = chdir(args[1]);
--- includes/builtin.h
+++ includes/builtin.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lalamino <lalamino@student.42.fr>          +#+  +:+       +#+        */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/21 11:58:05 by lalamino          #+#    #+#             */
-/*   Updated: 2026/05/26 11:30:44 by lalamino         ###   ########.fr       */
+/*   Updated: 2026/05/29 10:16:15 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ typedef struct s_int
 
 }	t_int;
 
+int	dash_lengh(char *str);
 int	builtin(t_ast *cmd, char ***env);
 void	env_bi(char **env);
 void	pwd(char **env);
