14f5a39c4632f8882a5b531f0916142abc4c4626
Kaito13th <freesamakkasetkan@hotmail.com>
2026-06-24 18:53:46 +0200

    fixed

--- lexer/main.c
+++ lexer/main.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/30 18:29:14 by csamakka          #+#    #+#             */
-/*   Updated: 2026/06/24 17:49:45 by marvin           ###   ########.fr       */
+/*   Updated: 2026/06/24 18:50:44 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,14 +94,15 @@ int	main(int argc, char **argv, char **envi)
 		tokens = tokenize(data.cmd, envp[0]);
 		free(data.cmd);
 		ast = parser(tokens);
-		if (!ast)
-			break ;
-		data.root_ast = ast;
-		free_tokens(tokens);
-		//print_ast(ast, 0);
-		executer(ast, envp[0], &data, 0);
-		free_ast(ast);
-		ast = NULL;
+		if (ast)
+		{
+			data.root_ast = ast;
+			free_tokens(tokens);
+			//print_ast(ast, 0);
+			executer(ast, envp[0], &data, 0);
+			free_ast(ast);
+			ast = NULL;
+		}
 	}
 	rl_clear_history();
 	if (envp[0][0])
